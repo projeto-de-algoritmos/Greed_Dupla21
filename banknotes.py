2c4408f259238cc3594e025ad7c3b867a751128b
DafneM <moretti.dafne@gmail.com>
2022-03-07 08:26:07 -0500

    Atualiza banknotes

--- banknotes.py
+++ banknotes.py
@@ -1,23 +1,12 @@
-N = int(input())
-print(N)
+valor_total = int(input())
+print(valor_total)
+notas = []
+i = 0
 
-print(N // 100, "nota(s) de R$ 100,00")
-N = N - (N//100)*100
-
-print(N // 50, "nota(s) de R$ 50,00")
-N = N - (N//50)*50
-
-print(N // 20, "nota(s) de R$ 20,00")
-N = N - (N//20)*20
-
-print(N // 10, "nota(s) de R$ 10,00")
-N = N - (N//10)*10
-
-print(N // 5, "nota(s) de R$ 5,00")
-N = N - (N//5)*5
-
-print(N // 2, "nota(s) de R$ 2,00")
-N = N - (N//2)*2
-
-print(N // 1, "nota(s) de R$ 1,00")
-N = N - (N//1)*1
+numbers = [100, 20, 50, 10, 5, 2, 1]
+numbers.sort(reverse = True)
+for j in range(len(numbers)):
+    qtd_notas = valor_total//numbers[j]
+    valor_total -= qtd_notas*numbers[j]
+    notas.append(qtd_notas)
+print(f"{notas[0]} nota(s) de R$ 100,00\n{notas[1]} nota(s) de R$ 50,00\n{notas[2]} nota(s) de R$ 20,00\n{notas[3]} nota(s) de R$ 10,00\n{notas[4]} nota(s) de R$ 5,00\n{notas[5]} nota(s) de R$ 2,00\n{notas[6]} nota(s) de R$ 1,00")
